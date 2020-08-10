82f7ffdcf1942a9dba3d3bff05eb6964bdaf45ce
Sandipan Saha <sandipansaha@Sandipans-MacBook-Pro.local>
2020-08-10 09:04:42 +0530

    Bottom Appbar with FAB

--- lib/screens/landing_screen.dart
+++ lib/screens/landing_screen.dart
@@ -4,7 +4,7 @@ import 'foundation.dart';
 import 'profile_page.dart';
 import 'add_symptoms_page.dart';
 import 'home_page.dart';
-
+import 'package:caringhopesapp/components/fab_bottom_app_bar.dart';
 
 
 class LandingScreen extends StatefulWidget {
@@ -29,59 +29,35 @@ class _LandingScreenState extends State<LandingScreen> {
 
   List<Widget> tabs = [
     HomePage(),
-    SymptomsPage(),
     ProfilePage(),
   ];
 
   @override
   Widget build(BuildContext context) {
-    if (isIos) {
-      return CupertinoTabScaffold(
-          tabBar: CupertinoTabBar(items: [
-            BottomNavigationBarItem(
-                icon: Icon(CupertinoIcons.home), title: Text("Home")),
-            BottomNavigationBarItem(
-                icon: Icon(CupertinoIcons.search), title: Text("Search")),
-            BottomNavigationBarItem(
-                icon: Icon(CupertinoIcons.person), title: Text("User Info"))
-          ]),
-          tabBuilder: (context, index) {
-            switch (index) {
-              case 0:
-                return HomePage();
-                break;
-              case 1:
-                return SymptomsPage();
-                break;
-              case 2:
-                return ProfilePage();
-                break;
-              default:
-                return HomePage();
-                break;
-            }
-          });
-    }
-    //Android Scafold
-    else {
       return Scaffold(
         appBar: AppBar(
+          backgroundColor: Color(0xff7165E2),
           title: Text(widget.title),
         ),
         // Body Where the content will be shown of each page index
         body: tabs[currentTabIndex],
-        bottomNavigationBar: BottomNavigationBar(
-            currentIndex: currentTabIndex,
-            onTap: onTapped,
-            items: [
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.home), title: Text("Home")),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.add), title: Text("Add")),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.person), title: Text("User Info"))
-            ]),
+        bottomNavigationBar: FABBottomAppBar(
+          color: Colors.grey,
+          selectedColor: Colors.black,
+          notchedShape: CircularNotchedRectangle(),
+          onTabSelected: onTapped,
+          items: [
+            FABBottomAppBarItem(iconData: Icons.home, text: 'Home'),
+            FABBottomAppBarItem(iconData: Icons.person, text: 'Profile'),
+          ],
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: FloatingActionButton(child: Icon(Icons.add),backgroundColor:Color(0xff7165E2),onPressed: (){
+          Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SymptomsPage()),
+          );
+        },)
       );
-    }
   }
 }
